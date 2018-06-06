7c312b5e2f4a9f1af4d9d18e816cb31e6ef41c5c
Thiyagarajan Veluchamy <thiyagarajannv@gmail.com>
2018-06-06 06:29:24 +0530

    minor

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:moorem/trusty-cms-app.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/pi/trusty"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
