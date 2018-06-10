1a6893420b30077ad48c021952ace34ace14e864
thiyagarajan <thiyagarajannv@yahoo.com>
2018-06-10 18:14:24 +0530

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,8 @@ TrustyCms::Application.configure do
 
 
   # Full error reports are disabled and caching is on
-  config.action_controller.consider_all_requests_local = false
-  config.action_controller.perform_caching             = true
+  config.consider_all_requests_local = false
+  config.perform_caching             = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host                  = "http://assets.example.com"
